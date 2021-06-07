459d43cb805d0724c4e74cf206f476915fa28bce
donghyun771 <dh031208@naver.com>
2021-06-07 20:15:01 +0900

    구글지도 API를 사용해 지도 액티비티 구현

--- app/src/main/java/com/example/healthpedometer/Pedometer.java
+++ app/src/main/java/com/example/healthpedometer/Pedometer.java
@@ -1,9 +1,9 @@
 package com.example.healthpedometer;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -11,15 +11,13 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.webkit.PermissionRequest;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import static androidx.core.content.PermissionChecker.PERMISSION_GRANTED;
 
@@ -32,7 +30,6 @@ public class Pedometer extends Activity implements SensorEventListener {
 
     //센서 연결을 위한 변수
     private SensorManager sensorManager;
-    //private Sensor accelerormeterSensor;
     private Sensor stepCountSensor;
 
     @Override
@@ -85,37 +82,33 @@ public class Pedometer extends Activity implements SensorEventListener {
         }
     }
 
-    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedometeter);
         OnCheckPermission();
-//        if(ContextCompat.checkSelfPermission(this,
-//                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
-//            //ask for permission
-//            ActivityCompat.requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, PERMISSIONS_REQUEST);
-//        }
         //센서 연결[걸음수 센서를 이용한 흔듬 감지]
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        //accelerormeterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
         final TextView tex = (TextView) findViewById(R.id.main_stepcount);
         ImageButton Start = (ImageButton) findViewById(R.id.main_imgBtn_start);
         ImageButton Stop = (ImageButton) findViewById(R.id.main_imgBtn_stop);
+        ImageButton Map = (ImageButton) findViewById(R.id.main_imgBtn_map);
         tex.setText(Steps +"걸음");
         // Required empty public constructor
         if (stepCountSensor == null) {
             Toast.makeText(this.getApplicationContext(), "No Step Detect Sensor", Toast.LENGTH_SHORT).show();
         }
         Start.setOnClickListener(v -> {
-//            final TextView text = (TextView) findViewById(R.id.main_stepcount);
-//            text.setText(Integer.toString(Steps));
             onStart();
         });
 
         Stop.setOnClickListener(v -> {
             onStop();
+        });
+        Map.setOnClickListener(v -> {
+            Intent intent1 = new Intent(this, MapsActivity.class);
+            startActivity(intent1);
         });
     }
 
@@ -132,44 +125,6 @@ public class Pedometer extends Activity implements SensorEventListener {
         }
     }
 
-
-
-//    public void Pedometer() {
-//
-//    }
-
-
-
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//        view = inflater.inflate(R.layout.activity_pedometeter, container, false);
-
-//        Start = (ImageButton) Start.findViewById(R.id.main_imgBtn_start);
-//        Stop = view.findViewById(R.id.main_imgBtn_stop);
-//        Walknum = view.findViewById(R.id.main_stepcount);
-
-
-//        if (stepCountSensor == null) {
-//            Toast.makeText(this.getApplicationContext(), "No Step Detect Sensor", Toast.LENGTH_SHORT).show();
-////                 Toast.makeText(this,"No Step Detect Sensor", Toast.LENGTH_SHORT).show();
-//        }
-
-
-//        Start.setOnClickListener(v -> {
-//
-//            onStart();
-//        });
-
-
-
-
-//        Stop.setOnClickListener(v -> {
-//            onStop();
-//        });
-
-
-//        return view;
-//    }
 
     public void onStart() {
         super.onStart();
@@ -192,31 +147,4 @@ public class Pedometer extends Activity implements SensorEventListener {
             Toast.makeText(this.getApplicationContext(), "Step Detect Sensor disconnect fail", Toast.LENGTH_SHORT).show();
         }
     }
-
-//    @Override
-//    public void onSensorChanged(SensorEvent event) {
-//        final TextView Walknum = (TextView) findViewById(R.id.main_stepcount);
-//        if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
-//            //stepcountsenersor는 앱이 꺼지더라도 초기화 되지않는다. 그러므로 우리는 초기값을 가지고 있어야한다.
-//            if (CounterSteps < 1) {
-//                // initial value
-//                CounterSteps = (int) event.values[0];
-//            }
-//            //리셋 안된 값 + 현재값 - 리셋 안된 값
-//            Steps = (int) event.values[0] - CounterSteps;
-//            CounterSteps++;
-//            Walknum.setText(Steps + "걸음");
-//            Log.i("log: ", "New step detected by STEP_COUNTER sensor. Total step count: " + Steps);
-//        }
-//        if (CounterSteps < 1) {
-//                // initial value
-//                CounterSteps = (int) event.values[0];
-//            }
-//        if (event.sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
-//            Steps++;
-//            walknum.setText(Steps + "걸음");
-//            Log.i("log: ", "New step detected by STEP_DETECTOR sensor. Total step count: " + Steps);
-//        }
-//    }
-
 }
